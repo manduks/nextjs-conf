bd919b76bdfe5479660ea53ce8d2224822300a55
Armando Gonzalez <iam@armando.mx>
2020-10-19 20:12:41 -0500

    Added code link

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,10 +1,11 @@
 export default function Button({ children, ...rest }) {
   return (
-    <button
+    <a
+      href="https://github.com/manduks/nextjs-conf" 
       className="bg-groovy p-4 text-white text-xl font-bold rounded-lg"
       {...rest}
     >
       {children}
-    </button>
+    </a>
   );
 }
