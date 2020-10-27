c990fca1d6d73359dc6cd0cd3d3fcf0c91ff2c37
Armando Gonzalez <iam@armando.mx>
2020-10-27 08:24:25 -0600

    updates from presentation

---
+++ components/SEO.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+const title = 'Responsive rendering - Next.js Conf';
+const description = 'In this talk we are going to talk on ways to optimize your assets load time by separating your components in a clear way, mobile, tablet and desktop or even views that are only useful for bots.';
+const url = 'www.armando.mx';
+
+export default function SEO() {
+  return (
+    <>
+      <link rel="icon" href="/favicon.ico" />
+      <meta property="og:site_name" content="Next.js conf 2020" />
+      <meta
+        property="og:image"
+        content="https://nextjs-conf.vercel.app/seo-card.jpg"
+      />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:url" content={url} />
+      <meta name="twitter:site" content="@manduks" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta
+        name="twitter:image"
+        content="https://nextjs-conf.vercel.app/seo-card.jpg"
+      />
+      <meta name="twitter:url" content={url} />
+    </>
+  );
+}
---
+++ pages/ssg.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import dynamic from "next/dynamic";
+import React from "react";
+import isMobile from "lib/isMobile";
+import SEO from "../components/SEO";
+import isBot from "lib/isBot";
+
+const Desktop = dynamic(() => import("composites/landing/Desktop"));
+const Mobile = dynamic(() => import("composites/landing/Mobile"));
+
+export default function Home() {
+  const isMobileDevice = isMobile();
+  const isBotDevice = isBot(); // dont render extra data if is a bot
+  return (
+    <>
+      <Head>
+        <title>Responsive Renderding</title>
+        <SEO />
+      </Head>
+      {!isBotDevice ? isMobileDevice ? <Mobile /> : <Desktop /> : null}
+    </>
+  );
+}
